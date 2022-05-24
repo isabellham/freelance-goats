6d034970a1cdfa59dfa3fcd473ae0332a3d650ba
Robert Gautsch <robert.gautsch93@gmail.com>
2022-05-24 15:04:10 +0200

    quicksave

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,14 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.save
+  end
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
--- app/controllers/goats_controller.rb
+++ app/controllers/goats_controller.rb
@@ -1,13 +1,22 @@
 class GoatsController < ApplicationController
   def index
+    @goats = Goat.all
   end
 
   def show
+    @goat = Goat.find(params[:id])
   end
 
   def new
+    @goat = Goat.new
   end
 
   def create
+    @goat = Goat.new(goat_params)
+    @goat.save
+  end
+
+  def goat_params
+    params.require(:goat).permit(:name, :type, :city, :breed, :gender, :description, :image_url, :price)
   end
 end
